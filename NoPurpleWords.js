2e2b26dec355f41e94712b499fca6223867dcc63
Bill <shirunjie@126.com>
2020-02-05 22:27:59 -0500

    Initial commit

---
+++ NoPurpleWords.js
@@ -0,0 +1,11 @@
+document.body.style.border = "5px solid red";
+// $('text[fill="#7030A0"]').attr({"fill": "#FFFFFF", "purple": true});
+setInterval(function() {
+	// let items = document.querySelectorAll('text[fill="#7030A0"]');
+	// for (let i = 0; i < items.length; i++) {
+	// 	console.log(items[i]);
+	// 	items[i].setAttribute("fill", "#FFFFFF");
+	// 	items[i].setAttribute("purple", true);
+	// }
+	$('text[fill="#7030A0"]').attr({"fill": "#FFFFFF", "purple": true});
+}, 1000);
