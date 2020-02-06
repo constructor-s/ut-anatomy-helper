12c7d2a874af571d0c0aa10c36854d580183818e
Bill <shirunjie@126.com>
2020-02-05 23:30:54 -0500

    Enable toggle

--- NoPurpleWords.js
+++ NoPurpleWords.js
@@ -1,15 +1,46 @@
 console.log("Anatomy Helper Running")
-document.body.style.border = "2px solid blue";
-// $('text[fill="#7030A0"]').attr({"fill": "#FFFFFF", "purple": true});
-setInterval(function() {
-	let items = document.querySelectorAll('text[fill="#7030A0"]');
-	for (let i = 0; i < items.length; i++) {
-		console.log(items[i]);
-		items[i].setAttribute("fill", "#FFFFFF");
-		items[i].setAttribute("purple", true);
-	}
-	// $('text[fill="#7030A0"]').attr({"fill": "#FFFFFF", "purple": true});
-}, 300);
+// document.body.style.border = "2px solid blue";
+// $('text[fill="#7030A0"]').attr({"fill": "#FFFFFF", "purplehidden": true});
+// setInterval(function() {
+// 	let items = document.querySelectorAll('text[fill="#7030A0"]');
+// 	for (let i = 0; i < items.length; i++) {
+// 		console.log(items[i]);
+// 		items[i].setAttribute("fill", "#FFFFFF");
+// 		items[i].setAttribute("purplehidden", true);
+// 	}
+// 	// $('text[fill="#7030A0"]').attr({"fill": "#FFFFFF", "purplehidden": true});
+// }, 300);
+
+function toggle(tab) {
+	console.log("Toggle!");
+	console.log(tab);
+	browser.tabs.executeScript({
+		code: `{
+			document.body.style.border = "10px solid blue";
+			let items = document.querySelectorAll('text[fill="#7030A0"]');
+			if (items.length > 0) {
+				console.log(items);
+				for (let i = 0; i < items.length; i++) {
+					console.log(items[i]);
+					items[i].setAttribute("fill", "#FFFFFF");
+					items[i].setAttribute("purplehidden", true);
+				}
+			} else {
+				let itemsHidden = document.querySelectorAll('text[purplehidden=true]');
+				console.log(itemsHidden);
+				for (let i = 0; i < itemsHidden.length; i++) {
+					console.log(itemsHidden[i]);
+					itemsHidden[i].setAttribute("fill", "#7030A0");
+					itemsHidden[i].removeAttribute("purplehidden");
+				}
+			}
+		}
+		`
+	});
+	// $('text[fill="#7030A0"]').attr({"fill": "#FFFFFF", "purplehidden": true});
+}
+
+browser.browserAction.onClicked.addListener(toggle);
 
 // // Select the node that will be observed for mutations
 // const targetNode = document.getElementById('presentation-container');
